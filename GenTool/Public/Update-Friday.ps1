94f6e298b1b2656f8d6117e98672e4c6d9a31979
Kim Laage <k.laage@gmail.com>
2020-01-17 09:05:31 +0100

    Add function Update-Friday

---
+++ GenTool/Public/Update-Friday.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+  Updates choco-installed software, installed PS-modules and the PS help files
+.DESCRIPTION
+  Update-Friday runs the "choco upgrade all -y" command, followed by an
+  "Update-Module" and "Update-Help"
+.EXAMPLE
+  PS C:\> Update-Friday
+#>
+function Update-Friday {
+  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+  param (
+  )
+  choco upgrade all -y 
+  Update-Module
+  Update-Help
+}
